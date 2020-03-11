2ba430e67b019255c2c7cfd88e3b96d221643eab
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-03-11 03:31:41 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,22 +1,3 @@
 class PigLatinizer 
   
-  attr_reader :phrase, :word 
-  
-  def initialize(phrase, word)
-    @phrase = phrase
-    @word = word
-  end 
-  
-  def piglatinize(phrase)
-    p = phrase.split(" ")
-    l = p.map {|
-  end 
-  
-  def piglatinize_word(word)
-  end 
-  
-  
-end 
-  
-  
   
