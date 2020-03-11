a6136f5d1fa4dbb482e20768aa09e5697d3b78da
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-03-11 03:45:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :user_input
   end 
   
-  post '/' do 
+  post '/piglatinize' do 
     @piglatinized = PigLatinizer.new(params[:user_input])
     erb :piglatinized 
   end 
